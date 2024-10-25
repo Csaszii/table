fc300d78a221d9277f22863f063f539b50634c16
Csaszar Oliver <csaszar.oliver@bolyai.local>
2024-10-25 07:59:27 +0200

    added form.reset

--- main.js
+++ main.js
@@ -27,197 +27,144 @@ let array = [
         pet: 'macska'
     },
 ]
- 
- 
- 
- 
+const tableBody = document.createElement("tbody")
 const table = document.createElement("table")
 const tableHeader = document.createElement("thead")
 const tableHeaderRow = document.createElement("tr")
- 
-createTableCell("td", "Vezetéknév", tableHeaderRow)
-createTableCell("td", "Keresztnév", tableHeaderRow)
-createTableCell("td", "Házas", tableHeaderRow)
-createTableCell("td", "Állat", tableHeaderRow)
 
+const tableHeaderRowLastName = document.createElement("th")
+tableHeaderRowLastName.innerHTML = "Vezetéknév"
 
-/* Fejléc sorok létrehozása */
-const tableHeaderRowLastname = document.createElement("th")
-tableHeaderRowLastname.innerHTML = "Vezetéknév"
- 
-const tableHeaderRowFirstname = document.createElement("th")
-tableHeaderRowFirstname.innerHTML = "Keresztnév"
- 
+const tableHeaderRowFirstName = document.createElement("th")
+tableHeaderRowFirstName.innerHTML = "Keresztnév"
+
 const tableHeaderRowMarried = document.createElement("th")
 tableHeaderRowMarried.innerHTML = "Házas"
- 
-const tableHeaderRowAnimal = document.createElement("th")
-tableHeaderRowAnimal.innerHTML = "Állat"
-/**/
- 
-tableHeaderRowFirstname.colSpan = 2
-const tableBody = document.createElement("tbody")
- 
- 
-document.body.appendChild(table)
- 
-table.appendChild(tableHeader)
- 
-tableHeader.appendChild(tableHeaderRow)
- 
-/* Fejléc sorok hozzáadása */
-tableHeaderRow.appendChild(tableHeaderRowLastname)
- 
-tableHeaderRow.appendChild(tableHeaderRowFirstname)
- 
-tableHeaderRow.appendChild(tableHeaderRowMarried)
- 
-tableHeaderRow.appendChild(tableHeaderRowAnimal)
-/***/
- 
-table.appendChild(tableBody)
- 
- 
- 
-const form = document.getElementById("form")
-form.addEventListener('submit', function(e){
-    tableBody.innerHTML=''
-        e.preventDefault()
-        const lastname = document.getElementById('lastname')
-        const firstname1 = document.getElementById('firstname1')
-        const firstname2 = document.getElementById('firstname2')
-        const married = document.getElementById('married')
-        const pet = document.getElementById('pet')
- 
-        const lastnameValue = lastname.value
-        const firstname1Value = firstname1.value
-        let firstname2Value = firstname2.value
-        const marriedChecked = married.checked
-        const petValue = pet.value
- 
-        if(firstname2Value === '')
-        {
-            firstname2Value = undefined
-        }
-       
-        if (validatefields(lastname, firstname1, pet)){
-            array.push({
-                lastname: lastnameValue,
-                firstname1: firstname1Value,
-                firstname2: firstname2Value,
-                married: marriedChecked,
-                pet: petValue,
-            })    
-        }
- 
-       
-       
-        RenderTable();
-       
- 
-        console.log(array)
- 
-})
- 
-RenderTable();
- 
-function RenderTable(){
-    for(const person of array){
-        person.lastname
-        const tr = document.createElement("tr")
-        tableBody.appendChild(tr)
 
-        tr.addEventListener('click', function(e){
-            console.log('click')
+const tableHeaderRowPet = document.createElement("th")
+tableHeaderRowPet.innerHTML = "Pet"
+
+
+tableHeaderRowFirstName.colSpan = 2
+
+const form = document.getElementById("form")
+form.addEventListener('submit', function (e) {
+    tableBody.innerHTML = ''
+    e.preventDefault()
+    const lastname = document.getElementById('lastname')
+    const firstname1 = document.getElementById('firstname1')
+    const firstname2 = document.getElementById('firstname2')
+    const married = document.getElementById('married')
+    const pet = document.getElementById('pet')
+
+    const lastnameValue = lastname.value
+    const firstname1Value = firstname1.value
+    let firstname2Value = firstname2.value
+    const marriedValue = married.checked
+    const petValue = pet.value
+
+    if (validatefields(lastname, firstname1, pet)) {
+        array.push({
+            lastname: lastnameValue,
+            firstname1: firstname1Value,
+            firstname2: firstname2Value || undefined,
+            married: marriedValue,
+            pet: petValue,
+        })
+    }
+
+    RenderTable()
+    console.log(array)
+
+    form.reset()
+})
+
+RenderTable()
+
+document.body.appendChild(table)
+table.appendChild(tableHeader)
+tableHeader.appendChild(tableHeaderRow)
+tableHeaderRow.appendChild(tableHeaderRowLastName)
+tableHeaderRow.appendChild(tableHeaderRowFirstName)
+tableHeaderRow.appendChild(tableHeaderRowMarried)
+tableHeaderRow.appendChild(tableHeaderRowPet)
+table.appendChild(tableBody)
+
+function RenderTable() {
+    tableBody.innerHTML = ''
+    for (const person of array) {
+        const tr = document.createElement("tr")
+
+        tr.addEventListener('click', function (e) {
             const selectedrow = tableBody.querySelector('.selected')
-            if(selectedrow != undefined)
-            {
+            if (selectedrow) {
                 selectedrow.classList.remove('selected')
             }
             e.currentTarget.classList.add('selected')
         })
-       
-        tr.innerHTML = person.lastname
 
-        const td = document.createElement("td")
-        td.innerHTML = person.firstname1
-        tr.appendChild(td)
-   
-       
-        if(person.firstname2 === undefined)
-        {
-            td.colSpan = 2
+        const tdLastName = document.createElement("td")
+        tdLastName.innerHTML = person.lastname
+        tr.appendChild(tdLastName)
+
+        const tdFirstName1 = document.createElement("td")
+        tdFirstName1.innerHTML = person.firstname1
+        tr.appendChild(tdFirstName1)
+
+        if (person.firstname2) {
+            const tdFirstName2 = document.createElement("td")
+            tdFirstName2.innerHTML = person.firstname2
+            tr.appendChild(tdFirstName2)
+        } else {
+            tdFirstName1.colSpan = 2
         }
-   
-        else
-        {
-            const td2 = document.createElement("td")
-            td2.innerHTML = person.firstname2
-            tr.appendChild(td2)
-        }
-   
-       
-        const td3 = document.createElement("td")
-        td3.innerHTML = person.married
-   
-   
-        if(td3.innerHTML == "false")
-        {
-            td3.innerHTML = "Nem"
-            tr.appendChild(td3)
-        }
-        else
-        {
-            td3.innerHTML = "Igen"
-            tr.appendChild(td3)
-        }
-   
-       
-        const td4 = document.createElement("td")
-       
-        td4.innerHTML = person.pet
-        tr.appendChild(td4)  
-    }    
+
+        const tdMarried = document.createElement("td")
+        tdMarried.innerHTML = person.married ? "Igen" : "Nem"
+        tr.appendChild(tdMarried)
+
+        const tdPet = document.createElement("td")
+        tdPet.innerHTML = person.pet
+        tr.appendChild(tdPet)
+
+        tableBody.appendChild(tr)
+    }
 }
- 
- 
-function validatefields(lastnamevali, firstname1vali, petvali){
+
+function validatefields(lastnamevali, firstname1vali, petvali) {
+    const errormessages = form.querySelectorAll('.error')
+    errormessages.forEach(error => error.innerHTML = '')
+
     let result = true
- 
-    if(lastnamevali.value === '')
-    {
+
+    if (lastnamevali.value === '') {
         let error = lastnamevali.parentElement.querySelector('.error')
         error.innerHTML = 'Vezetéknév kötelező!'
         result = false
     }
- 
-    else if(firstname1vali.value === '')
-    {
+    if (firstname1vali.value === '') {
         let error = firstname1vali.parentElement.querySelector('.error')
         error.innerHTML = 'Keresztnév kötelező!'
         result = false
     }
- 
-    else if(petvali.value === '')
-    {
+    if (petvali.value === '') {
         let error = petvali.parentElement.querySelector('.error')
-        error.innerHTML = 'Állat kötelező!'
+        error.innerHTML = 'Háziállat kötelező!'
         result = false
     }
- 
+
     return result
 }
-
-/**
- * 
-* @param {'td' | 'th'} tagName 
- * @param {string} innerHTML 
- * @param {HTMLTableRowElement} parent 
+    /**
+ *
+ * @param {'td' | 'th'} tagName
+ * @param {string} innerHTML
+ * @param {HTMLTableRowElement} parent
+ * @returns {HTMLTableCellElement}
  */
 
 function createTableCell(tagName, innerHTML, parent){
-    
-    const element = document.createElement("tagName")
-    element.innerHTML = innerHTML
+    const cell = document.createElement(tagName);
+    cell.innerHTML = innerHTML;
     parent.appendChild(cell)
 }
